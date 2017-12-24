f98696d50581f410da7de954fb95777a6ba8a4d8
Jim Fisher <jameshfisher@gmail.com>
2017-12-24 19:05:18 +0000

    handle posting a new event

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"os"
   "encoding/json"
+  "io/ioutil"
+  "fmt"
 )
 
 type Event struct {
@@ -12,12 +14,15 @@ type Event struct {
   RelativePoints int `json:"relativePoints"`
 }
 
-func events(w http.ResponseWriter, r *http.Request) {
+var events = []Event{
+  Event{ChildName: "sophie", RelativePoints: 1},
+  Event{ChildName: "constance", RelativePoints: 1},
+}
+
+func getEvents(w http.ResponseWriter, r *http.Request) {
+  fmt.Println("Serving events")
   w.Header().Set("Access-Control-Allow-Origin", "*")
-  j, err := json.Marshal([]Event{
-    Event{ChildName: "sophie", RelativePoints: 1},
-    Event{ChildName: "constance", RelativePoints: 1},
-  })
+  j, err := json.Marshal(events)
   if err != nil {
     http.Error(w, `JSON marshal failure`, 500)
     return
@@ -25,8 +30,35 @@ func events(w http.ResponseWriter, r *http.Request) {
 	w.Write(j)
 }
 
+func postEvent(w http.ResponseWriter, r *http.Request) {
+  fmt.Println("Received new event")
+  w.Header().Set("Access-Control-Allow-Origin", "*")
+  body, err := ioutil.ReadAll(r.Body)
+  if err != nil {
+    http.Error(w, `Could not read HTTP body`, 500)
+    return
+  }
+  var newEvent Event
+  err = json.Unmarshal(body, &newEvent)
+  if err != nil {
+    http.Error(w, `Could not unmarshal JSON from body`, 400)
+    return
+  }
+  events = append(events, newEvent)
+}
+
+func handleEvents(w http.ResponseWriter, r *http.Request) {
+  if r.Method == "GET" {
+    getEvents(w, r)
+  } else if r.Method == "POST" {
+    postEvent(w, r)
+  } else {
+    http.Error(w, `Method not allowed`, 405)
+  }
+}
+
 func main() {
-	http.HandleFunc("/events", events)
+	http.HandleFunc("/events", handleEvents)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
